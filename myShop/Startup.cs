fe7eb21301938ff88c606085d2c5a237394dfff8
Anatoly Zakharov <anatoly.v.zakharov@gmail.com>
2022-06-22 17:35:33 +0300

    First test commit

--- myShop/Startup.cs
+++ myShop/Startup.cs
@@ -27,7 +27,7 @@ namespace myShop
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!!!");
             });
         }
     }
